5dd859323fecc3cda014ac700024d5f88c0dafc6
allys7 <allys7@proton.me>
2022-09-14 22:32:27 -0600

    Finished treemap

--- in-class/ADT/treemap.ml
+++ in-class/ADT/treemap.ml
@@ -4,4 +4,4 @@ let rec map = fun f ->
   fun t -> (match t with 
         | Empty -> t
         | Node(lt,x,rt) -> Node(map f lt, f x, map f rt))
-in map (fun n -> n*2) Node(Empty,3,Node(Empty,2,Empty))
+in map (fun n -> n*2) (Node(Empty,3,Node(Empty,2,Empty)))
